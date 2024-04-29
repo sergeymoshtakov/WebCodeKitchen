33c2d839a4171b09ad29b06b6dbcc29cd7e4e943
Serhii Moshtakov <serhii.moshtakov@students.fhnw.ch>
2024-04-29 12:59:09 +0200

    added testing util documentation

--- SimpleCalculator/js/util/testingUtils.js
+++ SimpleCalculator/js/util/testingUtils.js
@@ -1,9 +1,14 @@
+/**
+ * Creates a test message based on the provided name and boolean value and writes it to the document.
+ * @param {string} name - The name of the test.
+ * @returns {Function} - A function that takes a boolean value and generates a test message accordingly.
+ */
 const test = name => bool => {
     let str = name;
-    if(bool){
+    if (bool) {
         str += " passed";
     } else {
         str += " not passed";
     }
     document.writeln(str);
-}
+};
