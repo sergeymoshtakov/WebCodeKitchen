9fc00ab8e4d073e98bedf2739d7fa48f8d352168
Serhii Moshtakov <serhii.moshtakov@students.fhnw.ch>
2024-04-29 13:02:28 +0200

    enchanced testing util documentation

--- SimpleCalculator/js/util/testingUtils.js
+++ SimpleCalculator/js/util/testingUtils.js
@@ -1,6 +1,7 @@
 /**
  * Creates a test message based on the provided name and boolean value and writes it to the document.
  * @param {string} name - The name of the test.
+ * @param {boolean} bool - The value of the assertion.
  * @returns {Function} - A function that takes a boolean value and generates a test message accordingly.
  */
 const test = name => bool => {
