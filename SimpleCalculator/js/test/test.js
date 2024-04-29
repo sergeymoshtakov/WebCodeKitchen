bcd156f5c3be9dfdef6598840b5d2893ab1fedfd
Serhii Moshtakov <serhii.moshtakov@students.fhnw.ch>
2024-04-29 12:57:49 +0200

    writen test cases

--- SimpleCalculator/js/test/test.js
+++ SimpleCalculator/js/test/test.js
@@ -1,1 +1,6 @@
-test("Addition of 1 to 2 ")(add(1)(2) === 3);
+test("Addition of 1 to 2 ")(add(1)(2) === 3); 
+test("Substraction of 3 from 5 ")(substract(5)(3) === 2);
+test("Multiplication of 3 and 5 ")(multiply(5)(3) === 15);
+test("Division of 14 by 7 ")(devide(14)(7) === 2); 
+test("Modus of 49 by 5 ")(mod(49, 5) === 4);
+test("Pow of 7 by 2 ")(pow(7)(2) === 49);
