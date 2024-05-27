15abec59d7c557ccd36121b4a9e440c5e348171c
Serhii Moshtakov <serhii.moshtakov@students.fhnw.ch>
2024-05-27 11:38:42 +0200

    added docu

--- functions testing/js/test.js
+++ functions testing/js/test.js
@@ -37,6 +37,10 @@ NullSafe(1).then(console.log);                   // will call the log
 NullSafe(null).then(console.log);                // will not call the log
 NullSafe(2).then(x => null).then(console.log);   // will not call the log
 
+/**
+ * Logs each argument passed to the function.
+ * @param {...*} arguments - The arguments to be logged.
+ */
 function foo(){
     for(a of arguments){
         console.log(a);
@@ -45,10 +49,17 @@ function foo(){
 
 foo(1);
 
+/**
+ * Creates an iterator with the specified start value, continuation condition, and increment function.
+ * @param {*} startValue - The initial value of the iterator.
+ * @param {Function} whileFn - The function to determine if the iteration should continue.
+ * @param {Function} incrementFn - The function to get the next value in the iteration.
+ * @returns {Object} - An iterator object.
+ */
 const Iterator = (startValue, whileFn, incrementFn) => {
     const next = () => {
         const proceed = whileFn(startValue);
-        let   value = undefined;
+        let value = undefined;
         if (proceed) {
             value = startValue;
             startValue = incrementFn(startValue);
@@ -58,20 +69,29 @@ const Iterator = (startValue, whileFn, incrementFn) => {
     return {
         [Symbol.iterator]: () => ({ next })
     };
-};   
+};
 
-const startValue  = 11;
-const whileFn     = i => i < 20;  
+const startValue = 11;
+const whileFn = i => i < 20;  
 const incrementFn = i => i + 2;
 
 for (const i of Iterator(startValue, whileFn, incrementFn)){
     console.log(i);
 }
 
-
+/**
+ * Generates an infinite sequence of odd numbers.
+ * @returns {Object} - An iterator for odd numbers.
+ */
 const oddNumbers = () => Iterator(1, x => true, x => x + 2);
-const _x = Math.floor( 1 + Math.random() * 100);
 
+const _x = Math.floor(1 + Math.random() * 100);
+
+/**
+ * Sums the first n numbers from the provided iterator.
+ * @param {number} n - The number of elements to sum.
+ * @returns {Function} - A function that takes an iterator and returns the sum of the first n numbers.
+ */
 const collect = n => oddNumbers => {
     let sum = 0;
     let count = 0;
@@ -87,7 +107,7 @@ const collect = n => oddNumbers => {
 };
 
 console.log(
-    collect(3)(oddNumbers())  ===  9 && 
-    collect(6)(oddNumbers())  === 36 &&
+    collect(3)(oddNumbers()) === 9 && 
+    collect(6)(oddNumbers()) === 36 &&
     collect(_x)(oddNumbers()) === _x * _x
 );
