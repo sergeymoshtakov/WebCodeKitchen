16f0d50e2d79f17e1d8d9a94d20e13812e93f4b9
Serhii Moshtakov <serhii.moshtakov@students.fhnw.ch>
2024-05-27 11:00:58 +0200

    fsdfdsdf

--- functions testing/js/test.js
+++ functions testing/js/test.js
@@ -36,3 +36,46 @@ const NullSafe = x => {
 NullSafe(1).then(console.log);                   // will call the log
 NullSafe(null).then(console.log);                // will not call the log
 NullSafe(2).then(x => null).then(console.log);   // will not call the log
+
+function foo(){
+    for(a of arguments){
+        console.log(a);
+    }
+}
+
+foo(1);
+
+const Iterator = (startValue, whileFn, incrementFn) => {
+    const next = () => {
+        const proceed = whileFn(startValue);
+        let   value = undefined;
+        if (proceed) {
+            value = startValue;
+            startValue = incrementFn(startValue);
+        }
+        return { value: value, done: !proceed };
+    };
+    return {
+        [Symbol.iterator]: () => ({ next })
+    };
+};   
+
+const startValue  = 11;
+const whileFn     = i => i < 20;  
+const incrementFn = i => i + 2;
+
+for (const i of Iterator(startValue, whileFn, incrementFn)){
+    console.log(i);
+}
+
+
+const oddNumbers = () => Iterator(1, x => true, x => x + 2);
+const _x = Math.floor( 1 + Math.random() * 100);
+
+const collect = n => oddNumbers => [...oddNumbers].slice(0,n).reduce((a,b) => a + b);
+
+console.log(
+    collect(3)(oddNumbers())  ===  9 && 
+    collect(6)(oddNumbers())  === 36 &&
+    collect(_x)(oddNumbers()) === _x * _x
+);
