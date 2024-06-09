020f610183df7f9ba6dd60814f1be103cbd56e8d
Serhii Moshtakov <serhii.moshtakov@students.fhnw.ch>
2024-06-09 16:22:12 +0200

    added docu

--- SimpleCalculator/js/util/currency-exchange.js
+++ SimpleCalculator/js/util/currency-exchange.js
@@ -1,5 +1,16 @@
+/**
+ * Retrieves the current exchange rate from the ExchangeRate-API for converting US Dollars (USD) to Swiss Francs (CHF).
+ * @async
+ * @function getExchangeRate
+ * @returns {Promise<number>} A promise that resolves with the current exchange rate between USD and CHF.
+ * @throws {Error} If there is an issue with fetching or parsing the exchange rate data.
+ */
 async function getExchangeRate() {
-    const response = await fetch('https://api.exchangerate-api.com/v4/latest/USD');
-    const data = await response.json();
-    return data.rates.CHF;
+    try {
+        const response = await fetch('https://api.exchangerate-api.com/v4/latest/USD');
+        const data = await response.json();
+        return data.rates.CHF;
+    } catch (error) {
+        throw new Error('Failed to fetch exchange rate data');
+    }
 }
